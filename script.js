27ca05fb4c8c59218f69c0da7cabd0b526383e0c
morning-bell-games <emily.morningbell@gmail.com>
2026-03-05 14:44:08 +1030

    tryfix

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const dropdownMenu = document.getElementById("dropdown-menu")
 navButton.addEventListener("click", () => {
     dropdownMenu.classList.toggle("visible")
     if (dropdownMenu.classList.contains("visible")) {
-        navButton.innerHTML = "<span class='arrow'>&#8593;</span>Games"
-    } else {
         navButton.innerHTML = "<em>></em>Games"
+    } else {
+        navButton.innerHTML = "<em>v</em> Games"
     }
 })
