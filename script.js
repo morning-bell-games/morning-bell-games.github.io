ef8f1fa841e6f352f5cb72b03d034e05635f2eaf
morning-bell-games <emily.morningbell@gmail.com>
2026-03-05 15:30:48 +1030

    arrow size

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const dropdownMenu = document.getElementById("dropdown-menu")
 navButton.addEventListener("click", () => {
     dropdownMenu.classList.toggle("visible")
     if (dropdownMenu.classList.contains("visible")) {
-        navButton.innerHTML = "<em><span class='arrow'>v</span></em> Games"
+        navButton.innerHTML = "<em><span class='arrow2'>v</span></em> Games"
     } else {
-        navButton.innerHTML = "<em><span class='arrow'>></span></em> Games"
+        navButton.innerHTML = "<em><span class='arrow1'>></span></em> Games"
     }
 })
