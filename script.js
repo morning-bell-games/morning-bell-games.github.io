b9d5d377d01c80cf9f7a667aaee4b55be1da5205
morning-bell-games <emily.morningbell@gmail.com>
2026-03-05 14:21:49 +1030

    innerhtml change

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ const dropdownMenu = document.getElementById("droppdown-menu")
 
 navButton.addEventListener("click", () => {
     dropdownMenu.classList.toggle("visible")
+    navButton.innerHTML = "Games"
 })
